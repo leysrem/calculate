7ad0e50083f7c5412ffd34d03cfdec8f7c6f8117
Dylan <dylan.hochet50@gmail.com>
2024-03-28 23:17:22 +0100

    Review 1

---
+++ calculateService.js
@@ -0,0 +1,45 @@
+const resultInput = document.querySelector("#result");
+
+function addition(value1, value2) {
+    resultInput.value = value1 + value2
+}
+function subtraction(value1, value2) {
+    resultInput.value = value1 - value2
+}
+function multiplication(value1, value2) {
+    resultInput.value = value1 * value2
+}
+function division(value1, value2) {
+    resultInput.value = value1 / value2
+}
+
+function onInit(){
+    // Listen operators on click
+    document.querySelectorAll('.operator').forEach(value => {
+
+        value.addEventListener('click', e => {
+            const value1 = parseInt(document.querySelector("#value1").value);
+            const value2 = parseInt(document.querySelector("#value2").value);
+
+            const operator = e.target.name
+            switch (operator) {
+                case '+':
+                    addition(value1, value2)
+                    break
+                case '-':
+                    subtraction(value1, value2)
+                    break
+                case '*':
+                    multiplication(value1, value2)
+                    break
+                case '/':
+                    division(value1, value2)
+                    break
+                default:
+                    throw Error('Operator not known')
+            }
+        })
+
+    })
+}
+onInit()
