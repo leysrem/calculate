bd5dbc054beec3207980621d52eeda552c8e4593
leysrem <remi.leyssene@gmail.com>
2024-04-06 17:53:09 +0200

    remi-4

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ class Calculate {
     }
 
     calculDelay(): Promise<true> {
+        //@ts-ignore
         return new Promise(function(resolve) {
             setTimeout((): void => {
                 resolve(true)
